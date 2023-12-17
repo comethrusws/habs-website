c0b73c9292f8de594d732df0bc958e2a2cf844ad
comethrusws <basabjha@naver.com>
2023-12-17 17:19:27 +0545

    Commit#3: Rough Footer

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -29,6 +29,40 @@
             </div>
         </div>
     </div>
+
+    <div class="footer-section">
+        <div class="ftcontainer">
+            <div class="bgimg">
+                <img src="../assets/footer-bg-2.jpg">
+            </div>
+            <div class="overlay">
+                <div class="footer-row">
+                    <div class="footer-column">
+                        <h3>Resources</h3>
+                            <ul>
+                                <li><a href="#">Calendar</a></li>
+                                <li><a href="#">Events</a></li>
+                                <li><a href="#">Activities</a></li>
+                            </ul>
+                    </div>
+                    <div class="footer-column">
+                        <h3>About Us</h3>
+                        <p>
+                        </p>
+                    </div>
+                    <div class="footer-column">
+                            <img src="../assets/menulogo.png">
+                            <p>
+                                Himalayan Academy Boarding School,<br>
+                                Sallaghari, Bhaktapur<br>
+                                +977 981-0136313<br>
+                                contact@habs.edu.np<br>
+                            </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -103,4 +137,77 @@ import NavBar from '../components/NavBar.vue'
     font-weight: 300;
 }
 
+
+
+.footer-section{
+    padding: 40px 0;
+}
+.ftcontainer{
+    width: 100%;
+    margin: auto;
+    position: relative;
+}
+.bgimg{
+    width: 100%;
+    height: 400px;
+    overflow: hidden;
+}
+.bgimg img{
+    width: 100%;
+    height: 400px;
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+}
+.overlay{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 1100px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.footer-row {
+  display: flex;
+  justify-content: space-between;
+  margin-left: 50px;
+  padding-bottom: 10px;
+}
+
+.footer-column {
+  flex: 0 0 50%;
+  margin-left: 40px;
+}
+
+.footer-column img {
+  height: 65px;
+  top: 0;
+  left: 0;
+  margin-bottom: 10px;
+  margin-left: -20px
+}
+.footer-column p{
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-weight: 300;
+
+}
+.footer-column ul {
+  margin-left: 40px;
+  list-style: none;
+  padding: 0;
+}
+
+.footer-column ul li {
+  margin-bottom: 10px;
+  margin-left: -20px;
+}
+
+.footer-column ul li a {
+  color: #000000;
+  text-decoration: none;
+}
 </style>
